721fa1a59c9195633d04b2ebf15f1f3e5d4d989f
kaipu1224 <kaipu1224@gmail.com>
2012-10-05 19:42:59 +0900

    画像フィルタを追加

--- examples/example08/example08.dart
+++ examples/example08/example08.dart
@@ -7,22 +7,38 @@ ImageElement img;
 
 void main() {
   canvas = query("#canvas");
-  canvas.width = window.innerWidth - 20;
-  canvas.height = window.innerHeight - 20;
-  
   img = new Element.tag("img");
-  img.src = "img01.jpg";
+  
+  var images = ["img01.jpg","img02.jpg","img03.jpg","img04.jpg","img05.jpg"];
+  SelectElement selectMenu = query("#imgSelect");
+  selectMenu.on.change.add((e){
+    SelectElement s = e.target;
+    selectImage(s.nodes[s.selectedIndex+1].text);
+  });
+  images.forEach((item) => selectMenu.nodes.add(new OptionElement(item, item, false, false)));
+  selectImage(images[0]);
+  
+  query("#grayBtn").on.click.add((e){
+    grayscaleFilter();
+  });
+  query("#sepiaBtn").on.click.add((e){
+    sepiaFilter();
+  });
+  query("#negaBtn").on.click.add((e){
+    negaFilter();
+  });
+}
+
+void selectImage(imageName){
+  img.src = imageName;
   img.on.load.add((e){
     drawImage();
-  });
-  
-  var gray = query("#convertBtn");
-  gray.on.click.add((e){
-    grayscaleFilter();
   });
 }
 
 void drawImage(){
+  canvas.width = img.width;
+  canvas.height = img.height;
   CanvasRenderingContext2D context = canvas.getContext("2d");
   context.fillStyle = "black";
   context.fillRect(0, 0, canvas.width, canvas.height);
@@ -33,17 +49,54 @@ void grayscaleFilter(){
   CanvasRenderingContext2D context = canvas.getContext("2d");
   var imgPixels = context.getImageData(0, 0, img.width, img.height);
   int pixLength = (img.width * img.height)*4;
-      
+  
+  int red, green, blue, gray;
   for(var i = 0; i < pixLength; i += 4){
-    int red = imgPixels.data[i];
-    int green = imgPixels.data[i+1];
-    int blue = imgPixels.data[i+2];
-    int gray = ((red + green + blue) / 3).toInt();
+    red = imgPixels.data[i];
+    green = imgPixels.data[i+1];
+    blue = imgPixels.data[i+2];
+    gray = (0.299 * red + 0.587 * green + 0.114 * blue).toInt();
     
     imgPixels.data[i] = gray;
     imgPixels.data[i+1] = gray;
     imgPixels.data[i+2] = gray;
   }
   context.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
-  context.save();
+}
+
+void sepiaFilter(){
+  CanvasRenderingContext2D context = canvas.getContext("2d");
+  var imgPixels = context.getImageData(0, 0, img.width, img.height);
+  int pixLength = (img.width * img.height)*4;
+  
+  int red, green, blue, sepia;
+  for(var i = 0; i < pixLength; i += 4){
+    red = imgPixels.data[i];
+    green = imgPixels.data[i+1];
+    blue = imgPixels.data[i+2];
+    sepia = (0.299 * red + 0.587 * green + 0.114 * blue).toInt();
+    
+    imgPixels.data[i] = (sepia/255*240).toInt();
+    imgPixels.data[i+1] = (sepia/255*200).toInt();
+    imgPixels.data[i+2] = (sepia/255*145).toInt();
+  }
+  context.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
+}
+
+void negaFilter(){
+  CanvasRenderingContext2D context = canvas.getContext("2d");
+  var imgPixels = context.getImageData(0, 0, img.width, img.height);
+  int pixLength = (img.width * img.height)*4;
+  
+  int red, green, blue;
+  for(var i = 0; i < pixLength; i += 4){
+    red = imgPixels.data[i];
+    green = imgPixels.data[i+1];
+    blue = imgPixels.data[i+2];
+    
+    imgPixels.data[i] = 255 - red;
+    imgPixels.data[i+1] = 255 - green;
+    imgPixels.data[i+2] = 255 - blue;
+  }
+  context.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
 }
